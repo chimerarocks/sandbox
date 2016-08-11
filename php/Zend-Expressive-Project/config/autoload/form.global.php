8659e339de2044eef0761ba01db4d3273c6224f4
jp <jaozinx@jp>
2016-08-11 11:26:49 -0300

    form view config

---
+++ php/Zend-Expressive-Project/config/autoload/form.global.php
@@ -0,0 +1,25 @@
+<?php
+
+$forms = [
+	'dependencies' => [
+		'aliases' => [
+
+		],
+		'invokables' => [
+		],
+		'factories' => [
+		]
+	],
+	'view_helpers' => [
+		'aliases' => [
+		],
+		'invokables' => [
+		],
+		'factories' => [
+		]
+	]
+];
+
+$configProviderForm = (new \Zend\Form\ConfigProvider())->__invoke();
+
+return \Zend\Stdlib\ArrayUtils::merge($configProviderForm, $forms);
