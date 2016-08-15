65760f80306242f4a494568ebb349fe466a47a08
JProg <joaopedrodslv@gmail.com>
2016-08-15 15:56:46 -0300

    chimerarocks tables refactoring

--- php/Laravel-TDD/application/ChimeraRocks/database/migrations/2016_08_13_1220_create_chimerarocks_categories_table.php
+++ php/Laravel-TDD/application/ChimeraRocks/database/migrations/2016_08_13_1220_create_chimerarocks_categories_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateChimeraRocksCategoriesTable
+class CreateChimerarocksCategoriesTable
 {
 	public function up()
 	{
