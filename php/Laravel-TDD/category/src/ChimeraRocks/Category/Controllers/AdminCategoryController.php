e02b2d1189a60f5710f36ecfcac267bf212a39c5
chimerarocks <joaopedrodslv@gmail.com>
2016-08-15 18:40:49 -0300

    refactorin admin category controller to better tet

--- php/Laravel-TDD/category/src/ChimeraRocks/Category/Controllers/AdminCategoryController.php
+++ php/Laravel-TDD/category/src/ChimeraRocks/Category/Controllers/AdminCategoryController.php
@@ -3,31 +3,34 @@
 namespace ChimeraRocks\Category\Controllers;
 
 use ChimeraRocks\Category\Models\Category;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Request;
 
 class AdminCategoryController extends Controller
 {
 	private $category;
+	private $response;
 
-	public function __construct(Category $category)
+	public function __construct(Category $categoryk, ResponseFactory $response)
 	{
 		$this->category = $category;
+		$this->response = $response;
 	}
 
 	public function index()
 	{
-		return view('chimeracategory::index', ['categories' => $this->category->all()]);
+		return $this->response->view('chimeracategory::index', ['categories' => $this->category->all()]);
 	}
 
 	public function create()
 	{
 		$categories = $this->category->all();
-		return view('chimeracategory::create', compact('categories'));
+		return $this->response->view('chimeracategory::create', compact('categories'));
 	}
 
 	public function store(Request $request)
 	{
 		$this->category->create($request->all());
-		return redirect()->route('admin.categories.index');
+		return $this->response->redirect()->route('admin.categories.index');
 	}
 }
