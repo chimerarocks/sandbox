22593f1e5c1782c10f05e571829f73340c2be0dc
chimerarocks <joaopedrodslv@gmail.com>
2016-08-16 20:17:34 -0300

    mock of all method

--- php/Laravel-TDD/database/tests/Test/Database/AbstractRepositoryTest.php
+++ php/Laravel-TDD/database/tests/Test/Database/AbstractRepositoryTest.php
@@ -15,4 +15,37 @@ class AbstractRepositoryTest extends AbstactTestCase
 
 		$this->assertInstanceOf(RepositoryInterface::class, $mock);
 	}
+
+	public function test_it_should_returns_all_whitout_arguments()
+	{
+		$mockRepository = Mockery::mock(AbstractRepository::class);
+		$mockStd = Mockery::mock(\stdClass::class);
+		$mockStd->id = 1;
+		$mockStd->name = 'name';
+		$mockStd->description = 'description';
+
+		$mockRepository
+		    ->shouldReceive('all')
+		    ->andReturn([$mockStd, $mockStd, $mockStd]);
+
+		$this->assertCount(3, $mockRepository->all());
+		$this->assertInstanceOf(\stdClass::class, $mockRepository->all()[0]);
+	}
+
+	public function test_it_should_returns_all_whit_arguments()
+	{
+		$mockRepository = Mockery::mock(AbstractRepository::class);
+		$mockStd = Mockery::mock(\stdClass::class);
+		$mockStd->id = 1;
+		$mockStd->name = 'name';
+		$mockStd->description = 'description';
+
+		$mockRepository
+		    ->shouldReceive('all')
+		    ->with(['id', 'name'])
+		    ->andReturn([$mockStd, $mockStd, $mockStd]);
+
+		$this->assertCount(3, $mockRepository->all(['id', 'name']));
+		$this->assertInstanceOf(\stdClass::class, $mockRepository->all(['id', 'name'])[0]);
+	}
 }
