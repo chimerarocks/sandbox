27d6cced991f88a9cdb66bf6974b4c78084e1af1
chimerarocks <joaopedrodslv@gmail.com>
2016-08-19 21:38:24 -0300

    Eloquent Repository

--- php/Laravel-TDD/category/src/ChimeraRocks/Category/Repositories/CategoryRepositoryEloquent.php
+++ php/Laravel-TDD/category/src/ChimeraRocks/Category/Repositories/CategoryRepositoryEloquent.php
@@ -4,9 +4,9 @@ namespace ChimeraRocks\Category\Repositories;
 
 use ChimeraRocks\Category\Models\Category;
 use ChimeraRocks\Category\Repositories\CategoryRepositoryInterface;
-use ChimeraRocks\Database\AbstractRepository;
+use ChimeraRocks\Database\AbstractEloquentRepository;
 
-class CategoryRepositoryEloquent extends AbstractRepository implements CategoryRepositoryInterface
+class CategoryRepositoryEloquent extends AbstractEloquentRepository implements CategoryRepositoryInterface
 {
 	public function model()
 	{
